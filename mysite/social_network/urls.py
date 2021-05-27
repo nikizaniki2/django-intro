977a81541e8330d1e209d93501ff39309be57d28
nikizaniki2 <nikizaniki2@gmail.com>
2021-05-27 16:13:22 +0300

    Added comments functionality

--- mysite/social_network/urls.py
+++ mysite/social_network/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import (PostListView, PostDetailView,
+ PostCreateView, PostUpdateView, PostDeleteView,
+ CommentCreateView, CommentUpdateView, CommentDeleteView)
 
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='sn-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='sn-detail'),
+    path('comment/<int:pk>/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete', CommentDeleteView.as_view(), name='comment-delete'),
     path('post/new/', PostCreateView.as_view(), name='sn-create'),
+    path('post/<int:pk>/comment/new', CommentCreateView.as_view(), name='comment-create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='sn-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='sn-delete'),
     path('about/', views.about, name='sn-about'),
--- mysite/social_network/views.py
+++ mysite/social_network/views.py
@@ -1,5 +1,7 @@
+from django.http import request
+from django.urls import reverse_lazy
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -37,9 +39,21 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date']
     paginate_by = 4
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # TODO: change from objects.all to objects.filter
+        #context['comments'] = Comment.objects.all()
+        return context
 
 class PostDetailView(DetailView):
     model = Post
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # TODO: change from objects.all to objects.filter
+        context['comments'] = Comment.objects.order_by('-date').filter(post=self.kwargs['pk'])
+        return context
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     success_url = '/'
@@ -50,5 +64,52 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return
 
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['content']
+    def form_valid(self, form):
+        form.instance.post = Post.objects.get(pk=self.kwargs['pk'])
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ['content']
+    def form_valid(self, form):
+        form.instance.post = Post.objects.get(pk=self.object.post_id)
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        comment = self.get_object()
+        if self.request.user == comment.author:
+            return True
+        return
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    success_url = '/post/{post_id}'
+    model = Comment
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return
+'''
+def CommentDetailView(request):
+    context = {
+        'comments': Comment.objects.all()
+    }
+    return render(request, PostDetailView.as_view(), context)
+'''
 def about(request):
     return render(request, 'social_network/about.html')
+
+'''
+# serializers.py
+
+class PostSerializer(serializer.ModelSerializer):
+    model = Post
+
+
+class PostView(mixins.ModelMixin):
+    serializer = PostSerializer
+    permmissions = (IsAuthenticatedPermisssion,)
+'''
