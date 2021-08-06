565cab176a48b4ddf244fa15904502352c1ed948
nikizaniki2 <nikizaniki2@gmail.com>
2021-08-06 14:43:41 +0300

    /user Now also returns ID of current user

--- mysite/users/restapi/urls.py
+++ mysite/users/restapi/urls.py
@@ -6,7 +6,7 @@ from rest_framework import routers, serializers, viewsets
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['url', 'id', 'username', 'email', 'is_staff']
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
