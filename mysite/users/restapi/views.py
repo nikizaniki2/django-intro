b401a3e807819b9d17fa075478da3dbe53d8fe33
nikizaniki2 <nikizaniki2@gmail.com>
2021-08-23 09:24:57 +0300

    Added pagination for UserPostsView (Profile View)

--- mysite/users/restapi/views.py
+++ mysite/users/restapi/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from comments.models import Post
 from rest_framework import viewsets
 from .serializers import UserPostsSerializer
+from rest_framework.pagination import PageNumberPagination
 
 @api_view(['GET'])
 def CurrentUserView(request):
@@ -12,7 +13,13 @@ def CurrentUserView(request):
         'username': user.username,
     })
 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 1
+    # page_size_query_param = 'page_size'
+    # max_page_size = 1000
+
 class UserPostsView(viewsets.ModelViewSet):
+    pagination_class = StandardResultsSetPagination
     def get_queryset(self):
         author_pk = self.kwargs['pk']
         return Post.objects.filter(author=author_pk)
